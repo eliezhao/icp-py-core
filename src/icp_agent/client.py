41bf97cc89a74fba4c52707bd9247310ff3f4311
C-B-Elite <czgelite@163.com>
2025-10-19 16:55:35 +0800

    update http client

--- src/icp_agent/client.py
+++ src/icp_agent/client.py
@@ -1,10 +1,13 @@
 # http client
 
+from __future__ import annotations
+
 import httpx
 from httpx import Timeout
 
 DEFAULT_TIMEOUT_SEC = 360.0
 DEFAULT_TIMEOUT = Timeout(DEFAULT_TIMEOUT_SEC)
+
 
 class Client:
     def __init__(self, url: str = "https://ic0.app"):
@@ -19,7 +22,7 @@ class Client:
         return resp.content
 
     def call(self, canister_id: str, data: bytes, *, timeout: Timeout = DEFAULT_TIMEOUT) -> httpx.Response:
-        # v3 endpoint
+        # v3 endpoint for update calls
         endpoint = f"{self.url}/api/v3/canister/{canister_id}/call"
         headers = {"Content-Type": "application/cbor"}
         resp = httpx.post(endpoint, content=data, headers=headers, timeout=timeout)
@@ -34,7 +37,7 @@ class Client:
     def status(self, *, timeout: Timeout = DEFAULT_TIMEOUT) -> bytes:
         endpoint = f"{self.url}/api/v2/status"
         resp = httpx.get(endpoint, timeout=timeout)
-        print("client.status:", resp.text)
+        # print("client.status:", resp.text)
         return resp.content
 
     # --------- async ---------
@@ -47,8 +50,9 @@ class Client:
             return resp.content
 
     async def call_async(self, canister_id: str, req_id: bytes, data: bytes, *, timeout: Timeout = DEFAULT_TIMEOUT) -> bytes:
+        # v3 endpoint (async)
         async with httpx.AsyncClient(timeout=timeout) as client:
-            endpoint = f"{self.url}/api/v2/canister/{canister_id}/call"
+            endpoint = f"{self.url}/api/v3/canister/{canister_id}/call"
             headers = {"Content-Type": "application/cbor"}
             await client.post(endpoint, content=data, headers=headers)
             return req_id
@@ -64,5 +68,5 @@ class Client:
         async with httpx.AsyncClient(timeout=timeout) as client:
             endpoint = f"{self.url}/api/v2/status"
             resp = await client.get(endpoint)
-            print("client.status:", resp.text)
+            # print("client.status:", resp.text)
             return resp.content
